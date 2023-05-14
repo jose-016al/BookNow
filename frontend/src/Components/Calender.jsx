f36d416cc75a29e29bf030f9bc698e03ea698511
jose_016al <jose_016al@outlook.com>
2023-05-14 18:17:41 +0200

    Modifiaciones en el componente de calendario

---
+++ frontend/src/Components/Calender.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+const Calender = ({ onDateChange }) => {
+
+    const handleDateChange = (date) => {
+        date = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
+        onDateChange(date);
+    };
+
+
+    // Lista de días a deshabilitar (festivos o específicos)
+    const diasDeshabilitados = [
+        new Date(2023, 4, 3), // 3 de mayo de 2023
+        // Agrega más días según tus necesidades
+    ];
+
+    const isDisabled = (date) => {
+        return (
+            isSunday(date) || // Deshabilitar domingos
+            diasDeshabilitados.some((disabledDate) => {
+                return (
+                    date.getDate() === disabledDate.getDate() &&
+                    date.getMonth() === disabledDate.getMonth() &&
+                    date.getFullYear() === disabledDate.getFullYear()
+                );
+            })
+        );
+    };
+
+    const isSunday = (date) => {
+        return date.getDay() === 0;
+    };
+
+    return (
+        <>
+            <div className='container'>
+                <Calendar onChange={handleDateChange} tileDisabled={({ date }) => isDisabled(date)} />
+            </div>
+        </>
+    );
+
+}
+
+export default Calender;
--- frontend/src/Routes/NewCita.jsx
+++ frontend/src/Routes/NewCita.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import Header from '../Components/Header';
+import Calender from '../Components/Calender';
+import 'react-calendar/dist/Calendar.css';
 
 const NewCita = () => {
-    
+
+    const handleDateChange = (date) => {
+        console.log(date);
+        // Realiza las acciones necesarias con la fecha seleccionada
+    };
+
     return (
         <>
-        <Header />
-        flipas
+            <Header />
+            <Calender onDateChange={handleDateChange}/>
         </>
     );
+
 }
 
 export default NewCita;
