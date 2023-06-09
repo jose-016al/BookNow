773130a220cc3dd6df849f61c3e6720461c84977
jose_016al <jose_016al@outlook.com>
2023-06-10 01:18:48 +0200

    Implemenatacion de big-calendar para citas confirmadas

--- frontend/src/Routes/BookingConfirmed.jsx
+++ frontend/src/Routes/BookingConfirmed.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'moment/locale/es';
 
 const BookingConfirmed = () => {
 
+    const localizer = momentLocalizer(moment);
     const [confirmed, setConfirmed] = useState([]);
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            fetchDatos();
-        }, 1000);
-        return () => clearInterval(interval);
+        fetchDatos();
     }, []);
 
     const fetchDatos = async () => {
@@ -20,53 +22,52 @@ const BookingConfirmed = () => {
             const json = await respuesta.json();
             const confirmadas = json.filter(booking => booking.status === 2);
             setConfirmed(confirmadas);
-        }
-        catch (error) {
+            console.log(json[0].formatDate(json[0].date.date) + 'T' + formatTime(json[0].time.date));
+        } catch (error) {
             console.log("error: " + error);
         }
-    }
+    };
 
     const formatDate = (dateString) => {
         const date = new Date(dateString);
         const year = date.getFullYear();
         const month = (date.getMonth() + 1).toString().padStart(2, '0');
         const day = date.getDate().toString().padStart(2, '0');
-        return `${day}/${month}/${year}`;
+        return `${year}-${month}-${day}`;
     };
 
     const formatTime = (timeString) => {
         const time = new Date(timeString);
         const hora = time.getHours();
         const minutos = time.getMinutes();
-        return `${hora.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}`;
+        const segundos = time.getSeconds();
+        return `${hora.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}:${segundos.toString().padStart(2, '0')}`;
     };
+
+    const BookingsCalendar = confirmed.map(cita => ({
+        title: `${cita.name} ${cita.last_name}`,
+        start: new Date(formatDate(cita.date.date) + 'T' + formatTime(cita.time.date)),
+        end: new Date(formatDate(cita.date.date) + 'T' + formatTime(cita.time.date)),
+    }));
 
     return (
         <>
             <Header />
-            <div className='container mt-4' style={{ minHeight: '310px' }}>
+            <div className='container mt-4' >
                 <h2>Citas confirmadas</h2>
-                <div className='mt-4 row justify-content-center align-items-center'>
-                    {confirmed && confirmed.length > 0 ? (
-                        confirmed.map(cita => (
-                            <div key={cita.id} className="card mb-5 mx-4 col-11 col-md-3" id='containerCita'>
-                                <div className="card-body">
-                                    <h5 className="card-title">{cita.name + " " + cita.last_name}</h5>
-                                    <p className="card-text mb-0">Fecha: <b>{formatTime(cita.time.date) + " " + formatDate(cita.date.date)}</b></p>
-                                    {cita.type.length > 0 && (
-                                        <p className="card-text mt-0">Tipo de cita: <b>{cita.type.map(type => type).join(', ')}</b></p>
-                                    )}  
-                                </div>
-                            </div>
-                        ))
-                    ) : (
-                        <h4 className='text-center'>No hay citas confirmadas</h4>
-                    )}
+                <div>
+                    <Calendar
+                        localizer={localizer}
+                        events={BookingsCalendar}
+                        startAccessor="start"
+                        endAccessor="end"
+                        style={{ height: 700 }}
+                    />
                 </div>
             </div>
             <Footer />
         </>
     );
-}
+};
 
 export default BookingConfirmed;
